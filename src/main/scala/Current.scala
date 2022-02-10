549eb59ccab80496b2ea713961ebbea08bf97e4a
Michael Arnaldi <arnaldimichael@gmail.com>
2022-02-10 12:01:03 +0000

    extend zio

--- src/main/scala/Current.scala
+++ src/main/scala/Current.scala
@@ -1,5 +1,17 @@
 import zio.*
 
+extension [R, E, A](zio: ZIO[R, E, A])
+  def provideServiceZIO[R1, E1, A1, Remainder](service: ZIO[R1, E1, A1])(
+      implicit
+      ev: A1 & Remainder => R,
+      ni: IsNotIntersection[A1],
+      tag: Tag[A1]
+  ): ZIO[R1 & Remainder, E | E1, A] = ZIO
+    .environmentWithZIO[R & R1](r =>
+      service.flatMap(s => zio.provideEnvironment(r.add(s)))
+    )
+    .asInstanceOf[ZIO[R1 & Remainder, E | E1, A]]
+
 object WithZIO {
   trait Console {
     def putStrLn(msg: => String): ZIO[Any, Nothing, Unit]
@@ -17,55 +29,16 @@ object WithZIO {
     def add(x: Int, y: Int) = ZIO.serviceWithZIO[Math](_.add(x, y))
   }
 
-  case class ErrorA(a: String)
-  case class ErrorB(b: String)
-
-  val programWithBadInference = for {
+  val program = for {
     _ <- Console putStrLn "hello"
     _ <- Console putStrLn "world"
-    y <- Math add(2, 3)
+    y <- Math add (2, 3)
     _ <- Console putStrLn s"result: $y"
-    _ <- ZIO fail ErrorA("a")
-    _ <- ZIO fail ErrorB("b")
-  } yield ()
+  } yield ("ok")
 
+  val makeMath = ZIO succeed new Math {
+    def add(x: Int, y: Int): ZIO[Any, Nothing, Int] = ZIO.succeed(x + y)
+  }
 
-  sealed trait AppErrors
-  case class AppErrorA(a: String) extends AppErrors
-  case class AppErrorB(b: String) extends AppErrors
-
-  val programWithOkInference = for {
-    _ <- Console putStrLn "hello"
-    _ <- Console putStrLn "world"
-    y <- Math add(2, 3)
-    _ <- Console putStrLn s"result: $y"
-    _ <- ZIO fail AppErrorA("a")
-    _ <- ZIO fail AppErrorB("b")
-  } yield ()
-}
-
-object RealLifeAppErrors {
-  sealed trait AppError
-  
-  sealed trait DatabaseError extends AppError
-  case class QueryFailure(msg: String) extends DatabaseError
-  case class ConnectionFailure(msg: String) extends DatabaseError
-
-  sealed trait DomainError extends AppError
-  case class InvalidUserId(msg: String) extends DomainError
-  case class InvalidEmail(msg: String) extends DomainError
-
-  def validateUserId(userId: String): ZIO[Any, InvalidUserId, Unit] = ???
-  def validateEmail(email: String): ZIO[Any, InvalidEmail, Unit] = ???
-
-  case class User(id: String, email: String)
-
-  def saveUser(user: User): ZIO[Any, QueryFailure, Unit] = ???
-
-  def createUser(id: String, email: String) = for {
-    _ <- validateUserId(id)
-    _ <- validateEmail(email)
-    user = User(id, email)
-    _ <- saveUser(user)
-  } yield (user)
+  val ok = program provideServiceZIO makeMath
 }
